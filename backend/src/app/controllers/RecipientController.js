0da048208178b968a8b54f0eea0866fa6712612d
Bruno Futema <bruno.futema@outlook.com>
2020-06-29 18:59:58 -0300

    feat: updating recipient

--- backend/src/app/controllers/RecipientController.js
+++ backend/src/app/controllers/RecipientController.js
@@ -40,6 +40,47 @@ class RecipientController {
       zip,
     });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zip: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails!' });
+    }
+
+    const { id } = req.body;
+
+    const recipient = await Recipient.findByPk(id);
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip,
+    });
+  }
 }
 
 export default new RecipientController();
