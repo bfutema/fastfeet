125c226eb8c6435c7f99fbd097962a57dd5d0c16
Bruno Futema <bruno.futema@outlook.com>
2020-06-29 19:40:28 -0300

    feat: add validation of deliverer

--- backend/src/app/controllers/DelivererController.js
+++ backend/src/app/controllers/DelivererController.js
@@ -1,7 +1,18 @@
+import * as Yup from 'yup';
+
 import Deliverer from '../models/Deliverer';
 
 class DelivererController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails!' });
+    }
+
     const deliverer = await Deliverer.create(req.body);
 
     return res.json(deliverer);
