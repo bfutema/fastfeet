cd4699abfb8da1bbc312d056313c3ee59f2acac0
Bruno Futema <bruno.futema@outlook.com>
2020-06-30 19:48:18 -0300

    feat: add list of orders

--- backend/src/app/controllers/OrderController.js
+++ backend/src/app/controllers/OrderController.js
@@ -1,13 +1,53 @@
 import * as Yup from 'yup';
 
 import Order from '../models/Order';
+import Recipient from '../models/Recipient';
+import DeliveryMan from '../models/DeliveryMan';
+import File from '../models/File';
 
 class OrderController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const orders = await Order.findAll({
+      attributes: ['id', 'product', 'cancelled_at', 'start_date', 'end_date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: Recipient,
+          as: 'recipient',
+          attributes: [
+            'id',
+            'name',
+            'street',
+            'number',
+            'complement',
+            'state',
+            'city',
+            'zip',
+          ],
+        },
+        {
+          model: DeliveryMan,
+          as: 'deliveryman',
+          attributes: ['id', 'name', 'email'],
+        },
+        {
+          model: File,
+          as: 'signature',
+          attributes: ['id', 'name', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json(orders);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       recipient_id: Yup.number().required(),
       deliveryman_id: Yup.number().required(),
-      signature_id: Yup.number().required(),
       product: Yup.string().required(),
     });
 
