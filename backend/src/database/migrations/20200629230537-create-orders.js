4df1b11f55266bb6d2e39b0befc653fca5d436ef
Bruno Futema <bruno.futema@outlook.com>
2020-06-29 22:44:37 -0300

    feat: changes in order migration

--- backend/src/database/migrations/20200629230537-create-orders.js
+++ backend/src/database/migrations/20200629230537-create-orders.js
@@ -38,11 +38,11 @@ module.exports = {
       },
       start_date: {
         type: Sequelize.DATE,
-        allowNull: false,
+        allowNull: true,
       },
       end_date: {
         type: Sequelize.DATE,
-        allowNull: false,
+        allowNull: true,
       },
       created_at: {
         type: Sequelize.DATE,
