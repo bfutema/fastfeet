da51275acb9df5edcfb20d638cec31bca501867e
Bruno Futema <bruno.futema@outlook.com>
2020-07-02 04:39:58 -0300

    refactor(frontend): remove dashboard, change redirect logged user to /orders directly

--- frontend/src/pages/Dashboard/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import api from '~/services/api';
-
-export default function Dashboard() {
-  api.get('recipients');
-  return <h1>Dashboard</h1>;
-}
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,7 +3,6 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import SignIn from '~/pages/SignIn';
-import Dashboard from '~/pages/Dashboard';
 import SaveDeliveryMan from '~/pages/DeliveryMan/Save';
 import DeliveryMans from '~/pages/DeliveryMan/List';
 import SaveOrder from '~/pages/Order/Save';
@@ -16,13 +15,12 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/dashboard" component={Dashboard} isPrivate />
-
-      <Route path="/save/deliveryman" component={SaveDeliveryMan} isPrivate />
-      <Route path="/deliverymans" component={DeliveryMans} isPrivate />
 
       <Route path="/save/order" component={SaveOrder} isPrivate />
       <Route path="/orders" component={Orders} isPrivate />
+
+      <Route path="/save/deliveryman" component={SaveDeliveryMan} isPrivate />
+      <Route path="/deliverymans" component={DeliveryMans} isPrivate />
 
       <Route path="/save/recipient" component={SaveRecipient} isPrivate />
       <Route path="/recipients" component={Recipients} isPrivate />
