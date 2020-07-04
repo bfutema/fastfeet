208f50c7d1cedb32758919ecd80716f7f6c39ae2
Bruno Futema <bruno.futema@outlook.com>
2020-07-04 10:56:21 -0300

    feat(mobile): configuring routes

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
     'no-console': ['error', { allow: ['tron'] }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'comma-dangle': 'off',
   },
   settings: {
     'import/resolver': {
